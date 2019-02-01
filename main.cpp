da8879b985a67879a7d34f794e8cde1ce7f97322
Kandroid <zhengkan29@163.com>
2019-02-02 00:21:55 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ int main(int argc, char* argv[]) {
 
     FILE *fp = fopen(filename, "rb");
     char ch;
+    ch = scan(fp);
+    if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_') {
+
+    }
     while((ch = scan(fp)) != -1) {
         putchar(ch);
+
     }
     puts("\n");
 
